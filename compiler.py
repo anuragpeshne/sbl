a5612195e1539eb92fbce7a0f9d3d158107fe84b
anuragpeshne <anurag.peshne@gmail.com>
2017-05-15 22:25:05 -0400

    broken parser, needs lexer

--- compiler.py
+++ compiler.py
@@ -2,36 +2,56 @@
 
 from sys import stdin
 
+class IDENT:
+    def __init__(string):
+        self.name = string
+
+class LAMBDA:
+    def __init__():
+        pass
+
 def isDigit(c):
     return ord(c) >= ord('0') and ord(c) <= ord('9')
+
+def scanNum(num, inp):
+    num = num * 10 + ord(inp[0])
+    if isDigit(inp[1:]):
+        return scanNum(num, inp[1:])
+    else:
+        return num
+
+def isAlpha(c):
+    return ((c[0] >= 'A' and c[0] <= 'Z') or
+            (c[0] >= 'a' and c[0] <= 'z'))
+
+def scanStr(string, inp):
+    string = string + inp[0]
+    if isAlpha(inp[0]):
+        return scanStr(string, inp[1:])
+    else:
+        return string
 
 # (+ 2 3)
 # parse-sexp
 # parse-nextExp
-def parse(inp, loc):
+def parse(inp):
     ast = []
-    while loc < len(inp) and inp[loc] != ')':
-        if inp[loc] == '(':
-            ast.append(parse(inp), loc + 1)
-        elif inp[loc] == ')':
-            return ast
-        elif isDigit(inp[loc]):
-            tokens.append(scanNum(inp, loc + 1))
-        elif isChar(inp[loc]):
-            string = scanStr()
-            if string == 'lambda':
-                tokens.append(LAMBDA)
-            else:
-                tokens.append(IDEN(string))
+    if (len(inp) <= 0 or inp[0] == ')'):
+        return ast
+    elif inp[0] == '(':
+        ast.append(parse(inp[1:]))
+    elif isDigit(inp[0]):
+        tokens.append(scanNum(0, inp))
+    elif isAlpha(inp[0]):
+        string = scanStr('', inp)
+        if string == 'lambda':
+            tokens.append(LAMBDA())
         else:
-            raise Error("unknown char" + inp[loc])
-
-    return ast
-
-def parse():
-    for token in tokens:
-        if token == LPAREN:
-            parse(tokens)
+            tokens.append(IDEN(string))
+    else:
+        raise SyntaxError("unknown char" + inp[0])
 
 if __name__ == "__main__":
-    compile_()
+    inp = stdin.read()
+    ast = parse(inp)
+    print ast
